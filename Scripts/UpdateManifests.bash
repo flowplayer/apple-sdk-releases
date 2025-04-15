00356cf0951b46d933911c8c6c05546acbcd2f7d
Rashadatjou <rashadatjou.mehdi@gmail.com>
2025-04-15 16:35:45 +0200

    fix(script): string replace using sed fix

--- Scripts/UpdateManifests.bash
+++ Scripts/UpdateManifests.bash
@@ -17,18 +17,23 @@ echo "New version is: $NEW_VERSION"
 
 # Update Package.swift
 echo "Updating Package.swift..."
-sed -E -i '' 's/let version = ".+"/let version = "'$NEW_VERSION\"/ Package.swift
+sed -i -E "s/let version = \".*\"/let version = \"$NEW_VERSION\"/" Package.swift
 sed -i -E "s/checksum: \".*\"/checksum: \"$NEW_CHECKSUM\"/" Package.swift
 echo "New Package.swift:"
 cat Package.swift
 
 # Update CocoaPods
 echo "Updating FlowplayerSDK.podspec"
-sed -E -i '' 's/VERSION = ".+"/VERSION = "'$NEW_VERSION\"/ FlowplayerSDK.podspec
+sed -i -E "s/VERSION = \".*\"/VERSION = \"$NEW_VERSION\"/" FlowplayerSDK.podspec
 echo "New FlowplayerSDK.podspec"
 cat FlowplayerSDK.podspec
 
-# Remove generated Package.swift-E
+# Remove temp Package.swift-E
 if [ -f "Package.swift-E" ]; then
     rm "Package.swift-E"
+fi
+
+# Remove temp FlowplayerSDK.podspec-E
+if [ -f "FlowplayerSDK.podspec-E" ]; then
+    rm "FlowplayerSDK.podspec-E"
 fi
